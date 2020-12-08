f108318dc897d09fbd126560277b923e31bf42e1
touit <touitou@gmail.com>
2020-12-08 13:18:32 -0500

    add payment tableau

--- src/app/paymentservice.service.ts
+++ src/app/paymentservice.service.ts
@@ -33,67 +33,86 @@ export class PaymentService  {
 
 
 
-  calculateDownPayments(): Observable<any> {
-//["weekly","bi-weekly","bi-monthly","monthly"]
+  amortize(): Observable<any> {
+ //["weekly","bi-weekly","bi-monthly","monthly"]
+
+   let factor = 1 ;
+   if ( this.data.mortgageRepayEvery=="weekly")  frequency = 4.33;
+   if ( this.data.mortgageRepayEvery=="bi-weekly")  frequency = 2.17;
+   if ( this.data.mortgageRepayEvery=="bi-monthly") frequency = 2;
+
+   let params  = {
+          "taux": this.data.mortgageRatePerPeriod/frequency,
+          "freq": frequency,
+          "amort": this.data.mortgageAmort*frequency,
+          "emprunt": this.data.mortgageAmount
+      }
+
+       var headers = new HttpHeaders()
+       .append('Content-Type', 'application/json')
+       .append('Accept', 'application/json')
+       .append('Access-Control-Allow-Origin', 'http://localhost:4200')
+       .append('Access-Control-Allow-Methods', '*')
+
+       console.log("DATA : " + this.data.mortgageAmount);
 
 
-
-  let frequency =12;
-  if ( this.data.mortgageRepayEvery=="weekly")  frequency = 52;
-   if ( this.data.mortgageRepayEvery=="bi-weekly")  frequency = 26;
-   if ( this.data.mortgageRepayEvery=="bi-monthly") frequency = 24;
-
-
-  let params  = {
-      "CalculMiseDeFondVersement" : this.data.mortgageAmount,
-      "CalculEmpruntVersement":0,
-      "CalculAmortVersement":this.data.mortgageAmort,
-      "CalculTauxVersement":this.data.mortgageRatePerPeriod,
-      "CalculFreqVersement":frequency,
-      "TaxeProprieteVersement":0,
-      "TaxeEvalMunicipaleTotale":0,
-      "TaxeCityId":"",
-      "TaxeCalcConfigId": "default",
-      "EmpruntAmort":0,
-      "EmpruntTaux":0,
-      "EmpruntCalculFreq":12,
-      "EmpruntVersement":0
+       return this.http.post('https://www.centris.ca/Calculator/GetMortgageRepaymentsArray', params,{headers})
+       .pipe(
+         tap((response: any) => {
+         console.log(response);
+           return response;
+         }),catchError(err => {
+             console.log(err);
+             return of(null);
+               })
+         );
      }
 
-      var headers = new HttpHeaders()
-      .append('Content-Type', 'application/json')
-      .append('Accept', 'application/json')
-      .append('Access-Control-Allow-Origin', 'http://localhost:4200')
-      .append('Access-Control-Allow-Methods', '*')
 
-      console.log("DATA : " + this.data.mortgageAmount);
+  calculateDownPayments(): Observable<any> {
+     //["weekly","bi-weekly","bi-monthly","monthly"]
 
-
-
+       let frequency =12;
+       if ( this.data.mortgageRepayEvery=="weekly")  frequency = 52;
+        if ( this.data.mortgageRepayEvery=="bi-weekly")  frequency = 26;
+        if ( this.data.mortgageRepayEvery=="bi-monthly") frequency = 24;
 
 
+       let params  = {
+           "CalculMiseDeFondVersement" : this.data.mortgageAmount,
+           "CalculEmpruntVersement":0,
+           "CalculAmortVersement":this.data.mortgageAmort,
+           "CalculTauxVersement":this.data.mortgageRatePerPeriod,
+           "CalculFreqVersement":frequency,
+           "TaxeProprieteVersement":0,
+           "TaxeEvalMunicipaleTotale":0,
+           "TaxeCityId":"",
+           "TaxeCalcConfigId": "default",
+           "EmpruntAmort":0,
+           "EmpruntTaux":0,
+           "EmpruntCalculFreq":12,
+           "EmpruntVersement":0
+          }
+
+           var headers = new HttpHeaders()
+           .append('Content-Type', 'application/json')
+           .append('Accept', 'application/json')
+           .append('Access-Control-Allow-Origin', 'http://localhost:4200')
+           .append('Access-Control-Allow-Methods', '*')
+
+           console.log("DATA : " + this.data.mortgageAmount);
 
 
-
-
-
-
-
-
-
-
-
-
-
-      return this.http.post('https://www.centris.ca/Calculator/CalculMntVersement', params,{headers})
-      .pipe(
-        tap((response: any) => {
-        console.log(response);
-          return response;
-        }),catchError(err => {
-            console.log(err);
-            return of(null);
-              })
-        );
-    }
+           return this.http.post('https://www.centris.ca/Calculator/CalculMntVersement', params,{headers})
+           .pipe(
+             tap((response: any) => {
+             console.log(response);
+               return response;
+             }),catchError(err => {
+                 console.log(err);
+                 return of(null);
+                   })
+             );
+         }
 }
